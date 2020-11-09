fc1c0dd342979d431655b86d1fd8af062498dec6
gusta <gusta@localhost.localdomain>
2020-11-09 20:35:06 -0300

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         title: 'Comida, meu bom!',
         theme: ThemeData(
           primarySwatch: Colors.red,
-          accentColor: Colors.blueAccent,
+          accentColor: Colors.amberAccent,
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
           textTheme: ThemeData.light().textTheme.copyWith(
